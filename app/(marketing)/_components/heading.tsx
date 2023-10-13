e06115579686a208edf5b4558f97829b6cc99978
noor-codes <noorullah.ahmadzai76@gmail.com>
2023-10-13 22:24:42 +0430

    Heading Auth

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,9 +1,16 @@
 'use client'
 
+import Link from 'next/link'
+
 import { Button } from '@/components/ui/button'
+import { Spinner } from '@/components/spinner'
 import { ArrowRight } from 'lucide-react'
+import { SignInButton } from '@clerk/clerk-react'
+import { useConvexAuth } from 'convex/react'
 
 export default function Heading() {
+  const { isLoading, isAuthenticated } = useConvexAuth()
+
   return (
     <div className='max-w-3xl space-y-4'>
       <h1 className='text-3xl sm:text-5xl md:text-6xl font-bold'>
@@ -14,10 +21,29 @@ export default function Heading() {
         Jotion is the connected workspace where <br /> better, faster work happens
       </h3>
 
-      <Button>
-        Enter Jotion
-        <ArrowRight className='h-4 w-4 ml-2' />
-      </Button>
+      {isLoading && (
+        <div className='w-full flex items-center justify-center'>
+          <Spinner size='lg' />
+        </div>
+      )}
+
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href='/documents'>
+            Enter Jotion
+            <ArrowRight className='h-4 w-4 ml-2' />
+          </Link>
+        </Button>
+      )}
+
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode='modal'>
+          <Button>
+            Get Jotion free
+            <ArrowRight className='h-4 w-4 ml-2' />
+          </Button>
+        </SignInButton>
+      )}
     </div>
   )
 }
