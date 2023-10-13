9baf996341a042b634ebb98b5add986bb6315c42
noor-codes <noorullah.ahmadzai76@gmail.com>
2023-10-13 14:32:27 +0430

    Marketing pages

---
+++ app/(marketing)/_components/heroes.tsx
@@ -0,0 +1,5 @@
+import Image from 'next/image'
+
+export function Heroes() {
+  return <div>Heroes</div>
+}
---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,13 @@
+import Heading from './_components/heading'
+import { Heroes } from './_components/heroes'
+
+export default function MarketingPage() {
+  return (
+    <div className='min-h-full flex flex-col dark:bg-[#1F1F1F]'>
+      <div className='flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10'>
+        <Heading />
+        <Heroes />
+      </div>
+    </div>
+  )
+}
