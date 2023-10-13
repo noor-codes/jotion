49e0279f18f85bbdcf1ecc46e47125fc60186865
noor-codes <noorullah.ahmadzai76@gmail.com>
2023-10-13 17:55:13 +0430

    yarn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from '@/components/providers/theme-provider'
 import './globals.css'
 
 import type { Metadata } from 'next'
@@ -28,8 +29,18 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en'>
-      <body className={inter.className}>{children}</body>
+    <html lang='en' suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+          storageKey='jotion-theme'
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
