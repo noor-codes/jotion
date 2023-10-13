ddd36fa99c48cceb6ef9f320ab4cca41a0c40f5a
noor-codes <noorullah.ahmadzai76@gmail.com>
2023-10-13 22:08:35 +0430

    userButton

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -5,9 +5,10 @@ import { Logo } from './logo'
 import { Button } from '@/components/ui/button'
 import { Spinner } from '@/components/spinner'
 import { ModeToggle } from '@/components/mode-toggle'
-import { SignInButton } from '@clerk/clerk-react'
+import { SignInButton, UserButton } from '@clerk/clerk-react'
 import { useScrollTop } from '@/hooks/use-scroll-top'
 import { useConvexAuth } from 'convex/react'
+import Link from 'next/link'
 
 export const Navbar = () => {
   const { isAuthenticated, isLoading } = useConvexAuth()
@@ -36,6 +37,15 @@ export const Navbar = () => {
             <SignInButton mode='modal'>
               <Button size='sm'>Get Jotion free</Button>
             </SignInButton>
+          </>
+        )}
+
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant='ghost' size='sm' asChild>
+              <Link href='/documents'>Enter Jotion</Link>
+            </Button>
+            <UserButton afterSignOutUrl='/' />
           </>
         )}
         <ModeToggle />
