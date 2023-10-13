8a7c270718e76ba883699bf7356584d94bacf140
noor-codes <noorullah.ahmadzai76@gmail.com>
2023-10-13 20:54:34 +0430

    Clerk and Convex

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
-import { ThemeProvider } from '@/components/providers/theme-provider'
 import './globals.css'
-
 import type { Metadata } from 'next'
+
 import { Inter } from 'next/font/google'
+import { ThemeProvider } from '@/components/providers/theme-provider'
+import { ConvexClientProvider } from '@/components/providers/convex-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -31,15 +32,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='en' suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider
-          attribute='class'
-          defaultTheme='system'
-          enableSystem
-          disableTransitionOnChange
-          storageKey='jotion-theme'
-        >
-          {children}
-        </ThemeProvider>
+        <ConvexClientProvider>
+          <ThemeProvider
+            attribute='class'
+            defaultTheme='system'
+            enableSystem
+            disableTransitionOnChange
+            storageKey='jotion-theme'
+          >
+            {children}
+          </ThemeProvider>
+        </ConvexClientProvider>
       </body>
     </html>
   )
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: 'https://mutual-doberman-97.clerk.accounts.dev',
+      applicationID: 'convex'
+    }
+  ]
+}
