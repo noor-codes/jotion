bf6817de700d60dba8d12b4abf899f015b0c5af3
noor-codes <noorullah.ahmadzai76@gmail.com>
2023-10-15 08:56:54 +0430

    document schema and mutation

---
+++ convex/schema.ts
@@ -0,0 +1,17 @@
+import { v } from 'convex/values'
+import { defineSchema, defineTable } from 'convex/server'
+
+export default defineSchema({
+  documents: defineTable({
+    title: v.string(),
+    userId: v.string(),
+    isArchived: v.string(),
+    parentDocument: v.optional(v.id('documents')),
+    content: v.optional(v.string()),
+    coverImage: v.optional(v.string()),
+    icon: v.optional(v.string()),
+    isPublished: v.boolean()
+  })
+    .index('by_user', ['userId'])
+    .index('by_user_parent', ['userId', 'parentDocument'])
+})
