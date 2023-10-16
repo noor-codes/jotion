2af3ac2ece41ee894acfff308b34942f927b8609
noor-codes <noorullah.ahmadzai76@gmail.com>
2023-10-16 19:14:42 +0430

    trash

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -3,8 +3,9 @@
 import { cn } from '@/lib/utils'
 import { api } from '@/convex/_generated/api'
 import { Item } from './item'
-import { UserItem } from './user-item'
 import { toast } from 'sonner'
+import { TrashBox } from './trash-box'
+import { UserItem } from './user-item'
 import { usePathname } from 'next/navigation'
 import { useMutation } from 'convex/react'
 import { DocumentList } from './Document-list'
@@ -141,8 +142,7 @@ export default function Navigation() {
               <Item label='Trash' icon={Trash} />
             </PopoverTrigger>
             <PopoverContent className='p-0 w-72' side={isMobile ? 'bottom' : 'right'}>
-              {/* <TrashBox /> */}
-              <p>trash box</p>
+              <TrashBox />
             </PopoverContent>
           </Popover>
         </div>
