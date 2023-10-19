b25b24a06d85575b7cd773e46752ad440ec1587f
noor-codes <noorullah.ahmadzai76@gmail.com>
2023-10-19 15:10:51 +0430

    title and iconpicker

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,14 +1,9 @@
 'use client'
 
-import { useMutation, useQuery } from 'convex/react'
-import dynamic from 'next/dynamic'
-import { useMemo } from 'react'
-
+import { Toolbar } from '@/components/toolbar'
 import { api } from '@/convex/_generated/api'
 import { Id } from '@/convex/_generated/dataModel'
-// import { Toolbar } from '@/components/toolbar'
-// import { Cover } from '@/components/cover'
-import { Skeleton } from '@/components/ui/skeleton'
+import { useQuery } from 'convex/react'
 
 interface DocumentIdPageProps {
   params: {
@@ -17,33 +12,17 @@ interface DocumentIdPageProps {
 }
 
 const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
-  //   const Editor = useMemo(() => dynamic(() => import('@/components/editor'), { ssr: false }), [])
-
   const document = useQuery(api.documents.getById, {
     documentId: params.documentId
   })
 
-  const update = useMutation(api.documents.update)
-
-  const onChange = (content: string) => {
-    update({
-      id: params.documentId,
-      content
-    })
-  }
-
   if (document === undefined) {
     return (
       <div>
-        {/* <Cover.Skeleton /> */}
-        <div className='md:max-w-3xl lg:max-w-4xl mx-auto mt-10'>
-          <div className='space-y-4 pl-8 pt-4'>
-            <Skeleton className='h-14 w-[50%]' />
-            <Skeleton className='h-4 w-[80%]' />
-            <Skeleton className='h-4 w-[40%]' />
-            <Skeleton className='h-4 w-[60%]' />
-          </div>
-        </div>
+        Loading...
+        {/* <div className='md:max-w-3xl lg:max-w-4xl mx-auto mt-10'>
+          <div className='space-y-4 pl-8 pt-4'></div>
+        </div> */}
       </div>
     )
   }
@@ -53,12 +32,11 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
   }
 
   return (
-    <div className='pb-40'>
-      {/* <Cover url={document.coverImage} />
+    <div className='pb-40 text-white'>
+      <div className='h-[35vh]' />
       <div className='md:max-w-3xl lg:max-w-4xl mx-auto'>
         <Toolbar initialData={document} />
-        <Editor onChange={onChange} initialContent={document.content} />
-      </div> */}
+      </div>
     </div>
   )
 }
